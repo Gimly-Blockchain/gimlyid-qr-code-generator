839d5caa60e5c59b9ebcbe4f9105cf5fe39fd2a6
Bram ten Cate <btencate@sphereon.com>
2021-09-14 17:23:35 +0200

    ONTO-49 prettier

--- src/GimlyIDQRCode/index.tsx
+++ src/GimlyIDQRCode/index.tsx
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { QRProps } from '../types';
 
 export const GimlyIDQRCode: FC<QRProps> = (QRProps) => {
-  const {type, did, mode, redirectUrl, onGenerate, bgColor, fgColor, level, size, title} = QRProps;
+  const { type, did, mode, redirectUrl, onGenerate, bgColor, fgColor, level, size, title } = QRProps;
   const nonce = uuidv4();
   const value = `nonce=${nonce},type=${type},did=${did},mode=${mode},redirectUrl=${redirectUrl}`;
 
@@ -17,17 +17,8 @@ export const GimlyIDQRCode: FC<QRProps> = (QRProps) => {
       mode: mode,
       redirectUrl: redirectUrl,
       qrValue: value,
-    })
+    });
   }
 
-  return (
-    <QRCode
-      value={value}
-      bgColor={bgColor}
-      fgColor={fgColor}
-      level={level}
-      size={size}
-      title={title}
-    />
-  );
+  return <QRCode value={value} bgColor={bgColor} fgColor={fgColor} level={level} size={size} title={title} />;
 };
