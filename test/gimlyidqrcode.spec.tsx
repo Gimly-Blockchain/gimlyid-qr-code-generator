f46c675c7ea7fd6ad787f2626172b86b46ffd40d
Bram ten Cate <btencate@sphereon.com>
2021-09-15 14:30:31 +0200

    ONTO-49 test fixes

--- test/gimlyidqrcode.spec.tsx
+++ test/gimlyidqrcode.spec.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { expect } from 'chai';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import {GimlyIDQRCode} from '../src/GimlyIDQRCode';
@@ -10,7 +9,7 @@ Enzyme.configure({ adapter: new Adapter() });
 describe("<GimlyIDQRCode /> shallow rendering", () => {
   it("should have props", () => {
     const wrapper = shallow(<GimlyIDQRCode type={QRType.AUTHENTICATION} did={'did:example:123456789abcdefghi'} mode={QRMode.DID_AUTH_SIOP_V2} />);
-    expect(wrapper.props().value).includes('type=auth,did=did:example:123456789abcdefghi,mode=didauth')
+    expect(wrapper.props().value).toContain(`"type":"auth","did":"did:example:123456789abcdefghi","mode":"didauth"`)
   });
 });
 
