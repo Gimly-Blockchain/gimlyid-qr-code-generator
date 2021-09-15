fffc54e543846ccf54668a52a9fd84a5acb0b236
Bram ten Cate <btencate@sphereon.com>
2021-09-15 13:42:50 +0200

    ONTO-49 qr content in JSON format

--- src/GimlyIDQRCode/index.tsx
+++ src/GimlyIDQRCode/index.tsx
@@ -7,7 +7,7 @@ import { QRProps } from '../types';
 export const GimlyIDQRCode: FC<QRProps> = (QRProps) => {
   const { type, did, mode, redirectUrl, onGenerate, bgColor, fgColor, level, size, title } = QRProps;
   const nonce = uuidv4();
-  const value = `nonce=${nonce},type=${type},did=${did},mode=${mode},redirectUrl=${redirectUrl}`;
+  const value = `"nonce":"${nonce}","type":"${type}","did":"${did}","mode":"${mode}","redirectUrl":"${redirectUrl}"`;
 
   if (onGenerate) {
     onGenerate({
