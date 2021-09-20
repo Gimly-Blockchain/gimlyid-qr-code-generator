23acc672b24d1f7a7bb8631c32e3f7af44c6c292
Bram ten Cate <btencate@sphereon.com>
2021-09-20 10:53:18 +0200

    ONTO-49 replaced uuid package with short-uuid

--- src/GimlyIDQRCode/index.tsx
+++ src/GimlyIDQRCode/index.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react';
 import QRCode from 'react-qr-code';
-import { v4 as uuidv4 } from 'uuid';
+
+const short = require('short-uuid');
 
 import { QRProps } from '../types';
 
 export const GimlyIDQRCode: FC<QRProps> = (props: QRProps) => {
   const { type, did, mode, redirectUrl, onGenerate, bgColor, fgColor, level, size, title } = props;
-  const state = uuidv4();
+  const state = short.generate();
   const value = `"state":"${state}","type":"${type}","did":"${did}","mode":"${mode}","redirectUrl":"${redirectUrl}"`;
 
   if (onGenerate) {
